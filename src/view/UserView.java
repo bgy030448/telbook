2c4c0f4be8dcfe019276f63481ffebd99ace9f57
bgy030448 <bgy030448@gmail.com>
2026-04-21 10:49:36 +0900

    feat : View 처리 중 데이터 입력 부분 완성

--- src/view/UserView.java
+++ src/view/UserView.java
@@ -1,5 +1,6 @@
 package view;
 
+import exception.InputValidation;
 import exception.MyException;
 
 import java.util.Map;
@@ -13,34 +14,59 @@ public class UserView {
     }
 
     public void insert() throws MyException{
-        //검증 클래스 생성
+        // 검증 클래스 생성
         InputValidation validation = new InputValidation();
+        // 입력자료 저장을 위한 변수 선언
+        String name;
+        int age;
+        String phone;
+        String address;
 
-        // 이름, 나이, 주소, 전화번호
+        // 이름, 나이, 전화번호, 주소
         System.out.println("== 전화번호 등록 ==");
-        // 이름 올바른 값이 들어올 때까지 반복
+        // 이름 옳바른 값이 들어올 때 까지 반복
         boolean nameOk = false;
         do {
             try {
-            // 이름 : 무조건 한글만.. 중간공백 없이
-            System.out.println("이름 : ");
-            String name = scanner.next();
-            validation.nameCheck(name);
-            nameOk = true;
-        } catch (MyException e) {
-            System.out.println(e.getMessage());
+                // 이름 : 무조건 한글만.. 중간공백 없이
+                System.out.println("이름 : ");
+                name = scanner.next();
+                validation.nameCheck(name);
+                nameOk = true;
+            } catch (MyException e) {
+                System.out.println(e.getMessage());
+            }
         } while (! nameOk);
-        }
 
         // 나이 : 0세 ~ 120세 사이값
-        System.out.println("나이 : ");
-        int age = scanner.nextInt();
+        boolean ageOk = false;
+        do {
+            try {
+                System.out.println("나이 : ");
+                age = scanner.nextInt();
+                validation.ageCheck(age);
+                ageOk = true;
+            } catch (MyException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (! ageOk);
+
         // 주소
         System.out.println("주소 : ");
-        String address = scanner.next();
+        address = scanner.next();
+
         // 전화번호(010-XXXX-XXXX)
-        System.out.println("전화번호 : ");
-        String phone = scanner.next();
+        boolean phoneOk = false;
+        do {
+            try {
+                System.out.println("전화번호 : ");
+                phone = scanner.next();
+                validation.phoneCheck(phone);
+                phoneOk = true;
+            } catch (MyException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (! phoneOk);
     }
 
     public void update() {
